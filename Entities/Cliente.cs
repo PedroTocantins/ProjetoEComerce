3d6281a4fb77770ce0418a2d62e189b1fb04b0f4
PedroTocantins <pedrotocantins11s@gmail.com>
2021-08-02 15:58:51 -0300

    first commit

---
+++ Entities/Cliente.cs
@@ -0,0 +1,10 @@
+namespace AgeOfTechAPI.Entities
+{
+    public class Cliente
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+    }
+}
